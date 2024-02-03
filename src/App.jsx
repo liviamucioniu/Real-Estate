e611d4194c931396686c8450650d878b2ff5d401
liviamucioniu <liviageorgescu1@gmail.com>
2024-02-03 15:12:59 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import Companies from "./components/Companies/Companies";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import GetStarted from "./components/GetStarted/GetStarted";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Residencies from "./components/Residencies/Residencies";
+import Value from "./components/Value/Value";
+import "./App.css";
 
 function App() {
-  return (
-    <span>subscribe Zainkeepscode</span>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Hero />
+            <Companies />
+            <Residencies />
+            <Value />
+            <Contact />
+            <GetStarted />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
